27652c7158dca113cf7b3efe46a23e10784e0917
python060319 <51337287+python060319@users.noreply.github.com>
2019-08-05 19:25:23 +0300

    Update repr_str.py

--- repr_str.py
+++ repr_str.py
@@ -17,9 +17,25 @@ mickey = Person('Mickey', 3, 1.2)
 goofy = Person('goofy', 5, 2)
 
 print(mickey)
+print(goofy.__repr__())
+
+str(mickey)
+repr(mickey)
+print(mickey)
 print(goofy)
 
 # class Point
 #  init (x,y)  default x = 0 y = 0
 #  repr - python command for creation
 #  str - print point x and y
+class Point():
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f'Point({self.x}, {self.y})'
+    def __str__(self):
+        return f'Point x:{self.x} y:{self.y}'
+
+p1 = Point(3.5, 7.9)
+print(p1)
